cc991b333fce9b6698e781d23117a358a07890a0
Akash raj <114800476+Akash-raj-INT@users.noreply.github.com>
2025-02-10 22:18:25 +0530

    Time: 3 ms (15.28%), Space: 9.1 MB (41.76%) - LeetHub

--- 3447-clear-digits/3447-clear-digits.cpp
+++ 3447-clear-digits/3447-clear-digits.cpp
@@ -1,28 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 class Solution {
 public:
     string clearDigits(string s) {
-
-        stack<char> st;
-        string ans;
-
-        for (auto it : s) {
-            if (isdigit(it)) {
-                if (!st.empty())
-                    st.pop();
+        stack<char> stk;
+        
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] < '0' || s[i] > '9') {  
+                stk.push(s[i]);
             } else {
-                st.push(it);
+                if (!stk.empty()) {
+                    stk.pop();
+                }
             }
         }
 
-        if (st.empty())
-            return "";
-
-        while (!st.empty()) {
-            ans.push_back(st.top());
-            st.pop();
+        string ans = "";
+        while (!stk.empty()) {
+            ans += stk.top();
+            stk.pop();
         }
 
         reverse(ans.begin(), ans.end());
+        
         return ans;
     }
 };
