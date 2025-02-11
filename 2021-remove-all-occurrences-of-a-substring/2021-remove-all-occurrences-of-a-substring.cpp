62fa602736ab637f18eddeb2220074d2e93bcf0f
Akash raj <114800476+Akash-raj-INT@users.noreply.github.com>
2025-02-11 09:10:14 +0530

    Time: 0 ms (100%), Space: 9.3 MB (90.62%) - LeetHub

---
+++ 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string removeOccurrences(string s, string part) {
+        
+        while ((s.find(part)) != string::npos) {  
+            unsigned idx = s.find(part);   
+            
+            s.erase(idx, part.size());   
+        }
+        
+        return s;
+    }
+};
