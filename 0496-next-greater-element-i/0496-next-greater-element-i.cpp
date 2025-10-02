2555ce2e1cee5dd93828d2b5cf25d860535d9c19
Akash raj <114800476+Akash-raj-INT@users.noreply.github.com>
2025-10-02 11:47:56 +0530

    Time: 3 ms (42.22%), Space: 12.2 MB (91.05%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,28 +1,27 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> result;
+        vector<int> res;
 
-        for (int num : nums1) {
-            int next = -1; // default if no greater element found
-            bool found = false;
+        for (int i = 0; i < nums1.size(); i++) {
+            int nextGreater = -1; // default if no greater element found
 
-            // find the position of num in nums2
-            for (int i = 0; i < nums2.size(); i++) {
-                if (nums2[i] == num) {
-                    // look to the right for next greater
-                    for (int j = i + 1; j < nums2.size(); j++) {
-                        if (nums2[j] > num) {
-                            next = nums2[j];
-                            break;
-                        }
-                    }
+            // find the index of nums1[i] in nums2
+            int j = 0;
+            while (j < nums2.size() && nums2[j] != nums1[i])
+                j++;
+
+            // check elements after index j in nums2
+            for (int k = j + 1; k < nums2.size(); k++) {
+                if (nums2[k] > nums1[i]) {
+                    nextGreater = nums2[k];
                     break;
                 }
             }
-            result.push_back(next);
+
+            res.push_back(nextGreater);
         }
 
-        return result;
+        return res;
     }
 };
